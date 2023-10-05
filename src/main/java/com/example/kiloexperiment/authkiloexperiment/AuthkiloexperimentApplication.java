8f52af2c11cdb8767591f70f743ccddf6d46b05d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-10-05 17:10:00 +0100

    Generates Spring Boot project with dependencies

---
+++ src/main/java/com/example/kiloexperiment/authkiloexperiment/AuthkiloexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiloexperiment.authkiloexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthkiloexperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthkiloexperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiloexperiment/authkiloexperiment/AuthkiloexperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiloexperiment.authkiloexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthkiloexperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
